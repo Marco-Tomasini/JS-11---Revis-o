f27f08d86a961186ac6308b66c205cff2f06bcc6
Marco_Tomasini <marco_tomasini@estudante.sesisenai.org.br>
2025-04-04 20:16:22 -0300

    ADD - ex5 to 8

--- script.js
+++ script.js
@@ -34,10 +34,43 @@ function VogalCons() {
     let pic = {"Chocolate":1.50,"Morango":2.50,"Creme":2.50,"Manga":3.20,"Melancia":3.40,"Vanilla Ice":3.00,"Céu Azul":3.60,"Brownie":4.00,"Hawaiano":5.00};
 
     console.log("Qual sabor de picolé deseja?");
-    for (let sabor in cardapio)
-        console.log(`- ${sabor}: R$${cardapio[sabor].toFixed(2)}`);
+    for (let sabor in pic) {
+        console.log("- " + sabor + ": R$" + pic[sabor]);
+    }
 
-  }
+    let escolha = prompt("Digite o sabor que deseja:");
+    let preco = pic[escolha];
 
+    console.log("O preço do picolé de " + escolha + " é R$" + preco);
+}
 
-  Picole();
+Picole();
+
+//ex8
+function adivinhe(){
+    rand = (Math.random() * 100) + 1;
+    rand = Math.floor(rand);
+    let resp = NaN
+
+    console.log("Que número acha que foi sorteado?");
+
+    while (resp !== rand){
+        resp = prompt("Digite um Número")
+
+        if(resp == rand){
+            console.log("Vc acertou o número, parabens: ", rand);
+            break;
+        } else{
+            let desistir = prompt("Vc errou o nº Deseja desistir?")
+
+            if(desistir == "sim"){
+             console.log("ok, o nº era:", rand);
+             break;
+            }
+        }
+
+        
+    }
+}
+
+adivinhe();
